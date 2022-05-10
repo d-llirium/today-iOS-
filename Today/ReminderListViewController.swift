57ea107df28b7be87e6cf6f5469b07f17d886bee
d-llirium <plimacarlos@brq.com>
2022-05-10 11:39:59 -0300

    configure data source

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ReminderListViewController: UICollectionViewController
 {
+    typealias DataSource = UICollectionViewDiffableDataSource< Int, String >
+    
+    var dataSource = DataSource!
 
     override func viewDidLoad()
     {
@@ -16,19 +19,41 @@ class ReminderListViewController: UICollectionViewController
         
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
+        
+        let cellRegistration = UICollectionView.CellRegistration // specifies how to configure the content and the appearence of the cell
+        { ( cell: UICollectionViewListCell
+            , indexPath: IndexPath
+            , itemIdentifier: String ) in
+            
+            let reminder = Reminder.sampleData[ indexPath.item ]
+            
+            var contentConfiguration = cell.defaultContentConfiguration() // creates a content configuration with the predefined system style
+            contentConfiguration.text = reminder.title // the list displays the configuration text as the primary text of a cell
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        dataSource = DataSource(
+            collectionView: collectionView )
+        { ( collectionView: UICollectionView
+            , indexPath: IndexPath
+            , itemIdentifier: String ) in // configure and returns a cell for a collection view
+            
+            return collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration
+                , for: indexPath
+                , item: itemIdentifier )
+        }
     }
 
-    func listLayout() -> UICollectionViewCompositionalLayout
+    private func listLayout() -> UICollectionViewCompositionalLayout
     {
-        var listConfiguration = UICollectionLayoutListConfiguration(
-            appearance: .grouped
-        ) // creates a section in a list layout
+        var listConfiguration = UICollectionLayoutListConfiguration( // creates a section in a list layout
+            appearance: .grouped )
         listConfiguration.showsSeparators = false
         listConfiguration.backgroundColor = .clear
         
         return UICollectionViewCompositionalLayout.list(
-            using: listConfiguration
-        )
+            using: listConfiguration )
     }
 
 }

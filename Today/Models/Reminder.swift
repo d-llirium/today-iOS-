c3123232b9b117d88e0005d0e6ba91b4dd79de8f
d-llirium <plimacarlos@brq.com>
2022-05-10 11:15:29 -0300

    format code and add comment about the if debug

--- Today/Models/Reminder.swift
+++ Today/Models/Reminder.swift
@@ -15,15 +15,36 @@ struct Reminder
     var isComplete: Bool = false
 }
 
-#if DEBUG
-extension Reminder {
+#if DEBUG //  is a compilation directive that prevents the enclosed code from compiling when you build the app for release. You can use this flag for testing code in debug builds — or for providing sample test data, like you’ll do in the next step.
+extension Reminder
+{
     static var sampleData =
     [
-        Reminder(title: "Get my glasses", dueDate: Date().addingTimeInterval(800.0), notes: "You have to pay for the service R$45.00")
-        , Reminder(title: "Transfer the light bill", dueDate: Date().addingTimeInterval(14000.0), notes: "try doing from the website")
-        , Reminder(title: "Practice clean code", dueDate: Date().addingTimeInterval(6000000.0), notes: "project claculator IMC")
-        , Reminder(title: "go to the clinic", dueDate: Date().addingTimeInterval(19000.0), notes: "make sure you have your documents")
-        , Reminder(title: "Game Presentation", dueDate: Date().addingTimeInterval(200000.0 ), notes: "get your shit together")
+        Reminder(
+            title: "Get my glasses"
+            , dueDate: Date().addingTimeInterval(
+                800.0 )
+            , notes: "You have to pay for the service R$45.00" )
+        , Reminder(
+            title: "Transfer the light bill"
+            , dueDate: Date().addingTimeInterval(
+                14000.0 )
+            , notes: "try doing from the website" )
+        , Reminder(
+            title: "Practice clean code"
+            , dueDate: Date().addingTimeInterval(
+                6000000.0 )
+            , notes: "project claculator IMC" )
+        , Reminder(
+            title: "go to the clinic"
+            , dueDate: Date().addingTimeInterval(
+                19000.0 )
+            , notes: "make sure you have your documents" )
+        , Reminder(
+            title: "Game Presentation"
+            , dueDate: Date().addingTimeInterval(
+                200000.0 ),
+            notes: "get your shit together" )
     ]
 }
 #endif
